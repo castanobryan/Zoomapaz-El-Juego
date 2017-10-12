fb9d4f607379c27431a71794a6a3966ad5b05135
castano_bryan <bryan_castano@hotmail.com>
2017-10-12 15:09:17 -0500

    Agrego movimiento bàsico y superficie

---
+++ Assets/CheckGround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	public class CheckGround : MonoBehaviour {
+
+		private PlayerController player;
+		// Use this for initialization
+		void Start () {
+			player = GetComponentInParent<PlayerController> ();
+		}
+
+		void OnCollisionStay2D(Collision2D col){
+			player.grounded = true;
+		}
+
+		void OnCollisionExit2D(Collision2D col){
+			player.grounded = false;
+		}
+	}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxSpeed = 5f;
+	public float speed = 2f;
+	public bool grounded;
+
+	public Rigidbody2D rb2d;
+	private Animator anim;
+
+
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		anim.SetFloat ("Speed", Mathf.Abs(rb2d.velocity.x));
+		//anim.SetBool ("Grounded", grounded);
+	}
+
+	void FixedUpdate () {
+
+		float h = Input.GetAxis ("Horizontal");
+
+		rb2d.AddForce (Vector2.right * speed * h);
+
+		float limitedSpeed = Mathf.Clamp (rb2d.velocity.x, -maxSpeed, maxSpeed);
+
+		rb2d.velocity = new Vector2 (limitedSpeed, rb2d.velocity.y);
+
+	}
+
+}
