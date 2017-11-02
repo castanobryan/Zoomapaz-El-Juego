33a593ff1c1325790ef2d009a4ee954ba7271d97
castano_bryan <bryan_castano@hotmail.com>
2017-11-01 20:25:19 -0500

    Movement SmallTiger

---
+++ Assets/CheckGroundedT.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGroundedT : MonoBehaviour {
+
+	private PlayerTigController player;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponentInParent<PlayerTigController> ();
+
+	}
+
+	void OnCollisionStay2D(Collision2D col){
+		if (col.gameObject.tag == "Ground") {
+			player.grounded = true;
+		}
+	}
+	void OnCollisionExit2D(Collision2D col){
+		if (col.gameObject.tag == "Ground") {
+			player.grounded = false;
+		}
+
+	}
+}
