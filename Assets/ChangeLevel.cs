5e1f9bbc8efee46f998dccf4885e14ed8af5844f
castano_bryan <bryan_castano@hotmail.com>
2017-11-06 17:48:44 -0500

    1W Ended, 60% background ended, ChangeScenes1-2

---
+++ Assets/ChangeLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeLevel : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.gameObject.tag == "Player"){
+			SceneManager.LoadScene ("2W", LoadSceneMode.Single);
+			Debug.Log ("funciona");
+		}
+	}
+
+	/*IEnumerator ChLevel(){
+		float fadeTime = GameObject.Find ("TriggerChangeLevel").GetComponent<FadeScene> ().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		SceneManager.LoadScene (sceneLoaded + 1, LoadSceneMode.Single);
+	}*/
+}
---
+++ Assets/FadeScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeScene : MonoBehaviour {
+
+	public Texture2D fadeOutTexture;
+	public float fadeSpeed = 0.8f;
+	private int drawDepth = -1000;
+	private float alpha = 1f;
+	private int fadeDir = -1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI(){
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;
+		alpha = Mathf.Clamp01 (alpha);
+
+		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+		GUI.depth = drawDepth;
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height),fadeOutTexture);
+	}
+
+	public float BeginFade (int direction){
+		fadeDir = direction;
+		return(fadeSpeed);
+	}
+
+	void OnLevelWasLoaded(){
+		BeginFade (-1);
+	}
+}
