225aafe439971cab2aff0b8c80ffa8d4e20ce22e
castano_bryan <bryan_castano@hotmail.com>
2017-11-09 21:37:04 -0500

    Main Menu, mute/unmute sound, buttons

---
+++ Assets/FuncionBotones.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FuncionBotones : MonoBehaviour {
+
+	public Button Boton;
+	public Sprite Mute;
+	public Sprite Unmute;
+	private int cont= 0;
+
+	void Start(){
+		Boton = GetComponent<Button> ();
+	}
+
+	public void IniciarJuego(){
+		SceneManager.LoadScene ("1W", LoadSceneMode.Single);
+	}
+
+	public void MutearTodo(){
+		AudioListener.pause = !AudioListener.pause;
+	}
+
+	public void SwitchButton(){
+		cont++;
+		if (cont % 2 == 0) {
+			Boton.image.overrideSprite = Mute;
+		} else {
+			Boton.image.overrideSprite = Unmute;
+		}
+	}
+}
